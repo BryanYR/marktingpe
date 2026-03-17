b118408effde77986f34239d07088dc369a502c9
BryanYR <bjyepez15@gmail.com>
2026-03-16 20:00:21 -0500

    feat: add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'Marketing Agency - Transformamos Ideas en Resultados',
+      title: 'Markting.pe - Transformamos Ideas en Resultados',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
